5f3f7f12a41bd2a34efdb3b0f1d0a36ef9496012
Miles Grover <miles@milesgrover.com>
2017-03-06 22:10:39 -0800

    making primary and secondary navs work with scrolling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,7 @@ function startScrolling(obj, spd, btn) {
         // recursively call startScrolling while mouse is pressed
         obj.animate({
             "scrollLeft": travel
-        }, 400, function () {
+        }, 200, function () {
             if (scrolling) {
                 startScrolling(obj, spd, btn);
             }
@@ -39,92 +39,128 @@ $.fn.checkSizes = function() {
 			checkAllItemsWidth += $(this).outerWidth(true);
 		});
 
+		// console.log(Math.round(checkAllItemsWidth) + ", " + Math.round(checkListWidth))
+
 		checkWidthsDiff = Math.round(checkAllItemsWidth) - Math.round(checkListWidth);
+	});
+}
+
+$.fn.buildNavScroll = function() {
+	$(this).each(function() {
+		var $this = $(this);
+		var $navList = $this.find("ul"); // find() because we're using container between wrapper and list
+
+		$this.checkSizes();
+
+		// Add left and right scroll buttons to the container within primary nav wrapper
+		// Buttons are display: none by default
+		$this.children()
+		.append("<button class='nav-pan-right'><span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span></button>")
+		.prepend("<button class='nav-pan-left'><span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></button>");
+
+		// console.log($(this).attr("class") + ", " + $(this).attr("data-nav") + ", " + checkWidthsDiff)
+		// Show the right scroll button if the list is too wide
+		if(checkWidthsDiff > 0) {
+			console.log($(this).attr("data-nav"))
+			$this.find(".nav-pan-right").css("display", "block");
+		}
+
+
+		// When you scroll the list (horizontally)
+		$navList.on("scroll", function() {
+			$this.checkSizes(); // check sizes again
+			listScrollLeft = Math.round($(this).scrollLeft()); // check scroll position
+
+			if (listScrollLeft == 0) { // list hasn't been scrolled
+			$this.find(".nav-pan-right").css("display", "block");
+			$this.find(".nav-pan-left").css("display", "");
+			scrolling = false; // stop scrolling function
+			}
+			else if (listScrollLeft == checkWidthsDiff) { // list is scrolled all the way
+				$this.find(".nav-pan-right").css("display", "");
+				$this.find(".nav-pan-left").css("display", "block");
+				setTimeout(function() {
+					// this timer is needed because when you initially click left arrow, this is still true and stops scrolling
+					if (listScrollLeft == checkWidthsDiff) {
+						scrolling = false; // stop scrolling function
+					}
+				}, 250);
+			}
+			else if (0 < listScrollLeft < checkWidthsDiff) { // list is scrolled somewhere in the middle
+				$this.find(".nav-pan-right").css("display", "block");
+				$this.find(".nav-pan-left").css("display", "block");
+			}
+
+			if (checkWidthsDiff <= 0) {
+				$this.find(".nav-pan-right").css("display", "");
+				$this.find(".nav-pan-left").css("display", "");
+			}
+		});
+	});
+
+	// When we click either the left or right scroll buttons
+	$(this).find(".nav-pan-left, .nav-pan-right").on("mousedown", function() {
+		var thisList = $(this).closest("nav").find("ul");
+		scrolling = true;
+
+		thisList.css({ // list overflow has to be visible to be scrolled
+			"overflow-x": "scroll",
+			"overflow-y": "hidden"
+		});
+
+		// Invoke scrolling function on this list,
+		// increment it by a good (arbitrary) amount,
+		// pass the button object so we can check what class it has
+		startScrolling(thisList, 66, $(this));
+
+		thisList.css("overflow", "hidden"); // we don't want the user to see the scroll bar, so hide it again
+
+	}).on("mouseup", function() {
+		scrolling = false; // stop scrolling function
+	});
+}
+
+$.fn.activateSecondary = function() {
+	$(this).each(function() {
+		$(this).on("click", "li a", function(e) {
+			e.preventDefault();
+			var target = $(this).attr("href").replace("#", "");
+
+			$(this).parent().siblings().addBack().removeClass("is-selected");
+			$(this).parent().addClass("is-selected");
+
+			$("[data-nav=" + target + "]").closest("nav").removeClass("is-selected");
+			$("[data-nav=" + target + "]").closest("nav").addClass("is-selected");
+			$("[data-nav=" + target + "]").siblings().removeClass("is-selected");
+			$("[data-nav=" + target + "]").addClass("is-selected");
+		});
 	});
 }
 
 // document loaded
 $(function () {
-	// Primary navigation
+	// navigation
     var $navPrimary = $(".nav-primary");
+    var $navSecondary = $(".nav-secondary");
 
-	// Check sizes of nav on load
 	$navPrimary.checkSizes();
+	$navPrimary.buildNavScroll();
+	$navPrimary.activateSecondary();
 
-        // In case there are multiple, we do this on each nav-primary
-        $navPrimary.each(function() { // !!!!!!!!!!! TO DO !!!!!!!!!!!! make this into function to use for subnav too
-            var $this = $(this);
-            var $navList = $this.find("ul"); // find() because we're using container between wrapper and list
-
-            // Add left and right scroll buttons to the container within primary nav wrapper
-            // Buttons are display: none by default
-            $this.children()
-            .append("<button class='nav-pan-right'><span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span></button>")
-            .prepend("<button class='nav-pan-left'><span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></button>");
-
-            // Show the right scroll button if the list is too wide
-            if(checkWidthsDiff > 0) {
-                $(".nav-pan-right").css("display", "block");
-            }
-
-            // When you scroll the list (horizontally)
-            $navList.on("scroll", function() {
-                $navPrimary.checkSizes(); // check sizes again
-                listScrollLeft = Math.round($(this).scrollLeft()); // check scroll position
-
-                if (listScrollLeft == 0) { // list hasn't been scrolled
-                    $(".nav-pan-right").css("display", "block");
-                    $(".nav-pan-left").css("display", "");
-                    scrolling = false; // stop scrolling function
-                }
-                else if (listScrollLeft == checkWidthsDiff) { // list is scrolled all the way
-                    $(".nav-pan-right").css("display", "");
-                    $(".nav-pan-left").css("display", "block");
-                    setTimeout(function() {
-                        // this timer is needed because when you initially click left arrow, this is still true and stops scrolling
-                        if (listScrollLeft == checkWidthsDiff) {
-                            scrolling = false; // stop scrolling function
-                        }
-                    }, 250);
-                }
-                else if (0 < listScrollLeft < checkWidthsDiff) { // list is scrolled somewhere in the middle
-                    $(".nav-pan-right").css("display", "block");
-                    $(".nav-pan-left").css("display", "block");
-                }
-                
-                if (checkWidthsDiff <= 0) {
-                    $(".nav-pan-right").css("display", "");
-                    $(".nav-pan-left").css("display", "");
-                }
-        });
-    });
-
-    // When we click either the left or right scroll buttons
-    $(".nav-pan-left, .nav-pan-right").on("mousedown", function() {
-        var thisList = $(this).closest(".nav-primary").find("ul");
-        scrolling = true;
-
-        thisList.css({ // list overflow has to be visible to be scrolled
-            "overflow-x": "scroll",
-            "overflow-y": "hidden"
-        });
-
-        // Invoke scrolling function on this list,
-        // increment it by a good (arbitrary) amount,
-        // pass the button object so we can check what class it has
-        startScrolling(thisList, 48, $(this));
-
-        thisList.css("overflow", "hidden"); // we don't want the user to see the scroll bar, so hide it again
-
-    }).on("mouseup", function() {
-        scrolling = false; // stop scrolling function
-    });
+	$navSecondary.checkSizes();
+	$navSecondary.buildNavScroll();
 
 	// When the window is resized
     $(window).on("resize", function() {
 		$navPrimary.checkSizes(); // check sizes again
+		$navSecondary.checkSizes();
 
         $navPrimary.each(function() {
+            $(this).find("ul").scrollLeft(0);
+            $(this).find("ul").trigger("scroll");
+        });
+
+        $navSecondary.each(function() {
             $(this).find("ul").scrollLeft(0);
             $(this).find("ul").trigger("scroll");
         });
